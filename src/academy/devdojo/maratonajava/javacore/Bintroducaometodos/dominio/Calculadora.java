cbfeb18448a0147cc20b9f9a10eff84da6a25cfa
Geyson Silva <geysonsilvaferreira@gmail.com>
2025-06-17 16:38:39 -0300

    POO-métodos

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
+
+public class Calculadora {
+    public void somaTresNumeros(){
+        System.out.println(15 + 15 + 15);
+    }
+
+    public void subtraiDoisNumeros(){
+        System.out.println(45 - 15);
+    }
+}
