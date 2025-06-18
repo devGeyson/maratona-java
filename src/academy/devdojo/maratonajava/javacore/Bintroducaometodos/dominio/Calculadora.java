cf2b26850ac58df8f31db4171798545864876216
Geyson Silva <geysonsilvaferreira@gmail.com>
2025-06-18 18:01:55 -0300

    POO-ParametrosPrimitivos

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -1,37 +1,45 @@
 package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
 
 public class Calculadora {
-    public void somaTresNumeros(){
+    public void somaTresNumeros() {
         System.out.println(15 + 15 + 15);
     }
 
-    public void subtraiDoisNumeros(){   // <-- Sintaxe sem parâmetro
+    public void subtraiDoisNumeros() {   // <-- Sintaxe sem parâmetro
         System.out.println(45 - 15);
     }
 
-    public void multiplicaDoisNumeros(int num1, int num2){   // <-- Sintaxe com parâmetro
+    public void multiplicaDoisNumeros(int num1, int num2) {   // <-- Sintaxe com parâmetro
         System.out.println(num1 * num2);
     }
 
-    public double divideDoisNumeros (double num1, double num2){
-        if (num2 == 0){
+    public double divideDoisNumeros(double num1, double num2) {
+        if (num2 == 0) {
             return 0;
         }
-        return num1/num2;
+        return num1 / num2;
     }
 
-    public double divideDoisNumeros02 (double num1, double num2){
-        if (num2 == 0){
+    public double divideDoisNumeros02(double num1, double num2) {
+        if (num2 == 0) {
             return 0;
-        }else {
-        return num1/num2;
+        } else {
+            return num1 / num2;
         }
     }
 
-    public void imprimeDivisaoDeDoisNumeros (double num1, double num2){
-        if (num2 == 0){
+    public void imprimeDivisaoDeDoisNumeros(double num1, double num2) {
+        if (num2 == 0) {
             System.out.println("Não existe divisão por 0");
         }
         return;
+    }
+
+    public void alteraDoisNumeros(int numero1, int numero2) {
+        numero1 = 99;
+        numero2 = 33;
+        System.out.println("Dentro do método");
+        System.out.println("Num1 " + numero1);
+        System.out.println("Num2 " + numero2);
     }
 }
